fb2523767ccb71be6bb8fb704158c05ffeb4c54b
Adrian <adicirstei@gmail.com>
2012-09-16 22:01:45 +0300

    database routes

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var http = require('http');
+
+var db = require('./database.js');
+
 var app = express();
+db.setup({dburl: 'mongo'})
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/app'));
 app.use(app.router);
+
+
+app.get('/db/get/:uid/:col/:id?', db.get);
+app.post('/db/set/:uid/:col/:id?', db.set);
+app.del('/db/del/:uid/:col/:id?', db.del);
 
 
 http.createServer(app).listen(app.get('port'), function(){
